f887d5cf3e7b956cdecc1c04bf5ce36d70855c1a
Jard1n <84231965+Jard1n@users.noreply.github.com>
2023-11-05 23:43:33 +0800

    Add files via upload

---
+++ Scripts/ChinaIP.py
@@ -0,0 +1,21 @@
+# 导入requests模块
+import requests
+
+# 获取IPv4地址列表
+v4China = "https://raw.githubusercontent.com/cbuijs/ipasn/master/country-asia-china4.list"
+r = requests.get(v4China)
+
+# 读取原始IPv4地址文件并转换成单独行的IP-CIDR形式
+original_content = r.text.split('\n')
+ip_cidr_content = []
+
+for ip in original_content:
+    if ip.strip():  # 忽略空行
+        ip_cidr = "IP-CIDR," + ip.strip()  # 添加 "IP-CIDR," 前缀
+        ip_cidr_content.append(ip_cidr)
+
+# 写入单独行的"IP-CIDR,IP"形式的内容到新文件
+with open("IPv4.China.list", "w") as cidr_file:
+    for line in ip_cidr_content:
+        cidr_file.write(line + '\n')
+
